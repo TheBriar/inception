cbaf13d074f55c5d42243d4051c09fa7c1f7bef4
Briar <86962846+TheBriar@users.noreply.github.com>
2022-08-19 20:14:15 +0200

    shit

--- srcs/mariadb/setup.sql
+++ srcs/mariadb/setup.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
 
+CREATE USER 'notreguser'@'%'; --create user
+SET PASSWORD FOR 'notreguser'@'%' = PASSWORD("notregpassword"); --set his password
+GRANT ALL PRIVILEGES ON wordpress.* TO 'notreguser'@'%'; --give admin acces to the wordpress database
+FLUSH PRIVILEGES; -- flush priviliges thus setting them
+
 CREATE USER 'reguser'@'%';
 SET PASSWORD FOR 'reguser'@'%' = PASSWORD("regpassword");
-GRANT ALL PRIVILEGES ON wordpress.* TO 'reguser'@'%';
 FLUSH PRIVILEGES;
