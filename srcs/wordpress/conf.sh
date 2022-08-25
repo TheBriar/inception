819d06113b337a02ab166c61bcf4cad86e96f15c
Briar <86962846+TheBriar@users.noreply.github.com>
2022-08-25 18:17:10 +0200

    think were good

--- srcs/wordpress/conf.sh
+++ srcs/wordpress/conf.sh
@@ -4,10 +4,12 @@ while ! mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
     sleep 1
 done
 
+mv /tmp/index.html ../index.html
 wp --allow-root core download
 wp config create --allow-root --dbhost=mariadb --dbname=wordpress --dbuser=reguser --dbpass=regpassword 
-wp core install --allow-root --title="Wordpress" --admin_name="notuser" --admin_password="notpassword" --admin_email="ok@ok.nl" --url="https://localhost/wordpress" --skip-email
-wp user create user --allow-root userok@userok.nl --role=author --user_pass="password"
+wp core install --allow-root --title="Wordpress" --admin_name=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASS --admin_email="ok@ok.nl" --url="https://localhost/wordpress" --skip-email
+wp user create user --allow-root userok@userok.nl --role=author --user_pass=$WORDPRESS_USER_PASS
 wp theme install --allow-root inspiro --activate 
 
+echo "setup script done, wordpress running"
 /usr/sbin/php-fpm7.3 -F -R
