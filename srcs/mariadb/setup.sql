f5a6f6292f583590a44f766714fd6ac45b612bcf
Briar <86962846+TheBriar@users.noreply.github.com>
2022-08-25 18:25:57 +0200

    root pass word alter

--- srcs/mariadb/setup.sql
+++ srcs/mariadb/setup.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
 
+SET PASSWORD FOR 'reguser'@'%' = PASSWORD("securepassword");
+
+
 CREATE USER 'reguser'@'%';
 SET PASSWORD FOR 'reguser'@'%' = PASSWORD("regpassword");
 GRANT ALL PRIVILEGES ON wordpress.* TO 'reguser'@'%'; --give admin acces to the wordpress database
